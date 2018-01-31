8b567786ce28d1394f0963c482b32956769b8fe7
dab2013 <dab2013@live.com>
2018-01-30 19:37:39 -0500

    Project 1

--- Project1ParallelSoftware/Project1.c
+++ Project1ParallelSoftware/Project1.c
@@ -2,29 +2,30 @@
 #include "UserInfoStruct.h"
 
 void main() {
-	struct UserInfo newUser;
 	int numUsers;
 	FILE * fPointer;
-	fPointer = fopen(&fPointer, "UserInformation.txt", "w");
-
+	struct UserInfo *newUser;
 
 	printf("How many Users are being added: ");
 	scanf_s(" %d ", &numUsers);
+	newUser = malloc(sizeof(struct UserInfo)*numUsers);
 
 	for (int i = 0; i < numUsers; i++) {
-
 		printf("Enter in values for the new User:\n");
-		puts("\tUsername: ");
-		gets(newUser.username);
-		puts("\tEmail: ");
-		gets(newUser.email);
-		puts("\tPassword: ");
-		gets(newUser.password);
+		printf("\tUsername: ");
+		fgets(newUser[i].username, 20, stdin);
+		printf("\tEmail: ");
+		fgets(newUser[i].email, 20, stdin);
+		printf("\tPassword: ");
+		fgets(newUser[i].password, 20, stdin);
 
-		fprintf(fPointer, "%s %s %s \n",newUser.username, newUser.username, newUser.username);
+		//printf("%s \n", newUser[i].username);
 	}
 
-	fclose(fPointer);
-	printf("%s \n",newUser.username);
+	//fPointer = fopen_s(&fPointer, "UserInformation.txt", "w");
+	//fprintf(fPointer, "Users");//"%s %s %s \n",newUser.username, newUser.username, newUser.username);
+	//fclose(fPointer);
+
+	free(newUser);
 	return 0;
 }
